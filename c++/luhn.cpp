fd05dd08b24554fdcf8a9b4ccf0edc1389460228
BoneyC <Boneywzm@gmail.com>
2025-08-11 14:20:45 -0600

    first commit

--- c++/luhn.cpp
+++ c++/luhn.cpp
@@ -10,11 +10,13 @@ int main(){
     }else{
         orden = true;
     } 
-   int i,auxiliar,suma = 0;
+   int i;
+   int auxiliar;
+   int suma = 0;
    std::string digito;
    for( i = 0 ; i < numDigitos ; i++){//desde aca
     digito = codigo[i];
-    auxilia r= stoi(digito);
+    auxiliar = stoi(digito);
     if(( i & 1 ) == orden ){/*el "&1" es para saber si es par o impar la posicion y orden para saber*/
         auxiliar=auxiliar*2;//para seguir la estructura de tre true=true y false false=true 
         if(auxiliar>9){//"&1" donde estoy y orden en que posicion hacer
@@ -24,10 +26,11 @@ int main(){
     suma=suma+auxiliar;
    }/* hasta aca estan los calculos que validan luhn lo otro solo es validar dio divisble 
    entre 10 el resto solo es escabilidad a diferentes legth*/
+    //return suma % 10 == 0 ? false : true;
    if(suma % 10 == 0){
-    std::cout<<"su tarjeta\t"<<codigo<<"\tsi cumple el algoritmo de luhn";
+    return false;
    }else{
-    std::cout<<"su tarjeta\t"<<codigo<<"\tno cumple el algoritmo de luhn";
+    return true;
    }
-   return 0;
+   
 }
