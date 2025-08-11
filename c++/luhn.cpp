47fe85fccd4bd224b1e0a495927276bb4732c2ef
BoneyC <Boneywzm@gmail.com>
2025-08-11 13:44:09 -0600

    first commit

--- c++/luhn.cpp
+++ c++/luhn.cpp
@@ -3,19 +3,19 @@
 
 int main(){
     bool orden;
-    std::string codigo="4258812475889092";
-    int numDigitos=codigo.length();   
-        if(!(numDigitos & 1)){
+    std::string codigo = "4258812475889092";
+    int numDigitos = codigo.length();   
+        if(! (numDigitos & 1)){
         orden=false;
     }else{
-        orden=true;
+        orden = true;
     } 
-   int i,auxiliar,suma=0;
+   int i,auxiliar,suma = 0;
    std::string digito;
-   for(i=0;i<numDigitos;i++){//desde aca
-    digito=codigo[i];
-    auxiliar=stoi(digito);
-    if((i&1)==orden){/*el "&1" es para saber si es par o impar la posicion y orden para saber*/
+   for( i = 0 ; i < numDigitos ; i++){//desde aca
+    digito = codigo[i];
+    auxilia r= stoi(digito);
+    if(( i & 1 ) == orden ){/*el "&1" es para saber si es par o impar la posicion y orden para saber*/
         auxiliar=auxiliar*2;//para seguir la estructura de tre true=true y false false=true 
         if(auxiliar>9){//"&1" donde estoy y orden en que posicion hacer
             auxiliar=(auxiliar-9);
@@ -24,7 +24,7 @@ int main(){
     suma=suma+auxiliar;
    }/* hasta aca estan los calculos que validan luhn lo otro solo es validar dio divisble 
    entre 10 el resto solo es escabilidad a diferentes legth*/
-   if(suma%10==0){
+   if(suma % 10 == 0){
     std::cout<<"su tarjeta\t"<<codigo<<"\tsi cumple el algoritmo de luhn";
    }else{
     std::cout<<"su tarjeta\t"<<codigo<<"\tno cumple el algoritmo de luhn";
--- javascript/validacionLunh.js
+++ javascript/validacionLunh.js
@@ -1,83 +1,86 @@
-let valides=false;
-let i,contador=0,Añoactual=0,Mesactual=0,bandera,Añobin,Mesbin,Cvvbin,aux=0,Verificacionexp,CC;
-let validez=false
-const fechaActual=new Date();
-Añoactual=fechaActual.getFullYear();
-Mesactual=fechaActual.getMonth()+1;
-let CCtxt="372754888804053 | 05 | 2030 | 1015"//xxxx xxxx xxxx xxx / MM / YYYY /  CVVV
-CC=CCtxt.replace(/[^|\d/°_+\-]/g,'')
+let valides = false;
+let luhn = false;
+let i;
+let contador = 0
+let bandera;
+let Añobin;
+let Mesbin;
+let Cvvbin;
+let aux = 0;
+let Verificacionexp;
+const fechaActual = new Date();
+const Añoactual = fechaActual.getFullYear();
+const Mesactual = fechaActual.getMonth()+1;
+let CCtxt="372754888804053 | 05 | 2030 | 1015";//xxxx xxxx xxxx xxx / MM / YYYY /  CVVV
+const CC = CCtxt.replace(/[^|\d/°_+\-]/g,'');
 //28 caracteres divisores en 18/17 20/19 25/24
 
 
-if(CC[0]==3){//codigo principal 
-    bandera=true
-    amex(CC,contador)
-    if(Verificacionexp){
-        valides=true
+if(CC[0] == 3){//codigo principal 
+    bandera = true;
+    verificarExp();
+    if(Verificacionexp && luhn){
+        valides = true;
     }
-
 }else{
-    bandera=false
-   general(CC,contador)
-   if(Verificacionexp){
-    valides=true
-}
+    bandera = false;
+    verificarExp();
+   if(Verificacionexp && luhn){
+    valides = true;
+   }//valides seria el valor retornable del modulo 
 }/*aca ya se valido luhn y caducidad la cosa del cvv es innecesaria en
 la validacion solo lo puse partede la estructura para guiarme
 cosas como los slice se vuelven innecesarios en un entorno web donde el cuestionario
-recibe esos datos ya extraidos la declaracion de variables deberia mejorarla  
-*/
-
+recibe esos datos ya extraidos la declaracion de variables deberia mejorarla  */
 
 function verificarExp(){
-    Verificacionexp=false;
-if(bandera){//amex
-Añobin=parseInt(CC.slice((19),(24)))
-Mesbin=parseInt(CC.slice((16),(19)))
-if(Añobin>Añoactual ||(Añobin==Añoactual && Mesbin>Mesactual)){
-    Verificacionexp=true; 
+    Verificacionexp = false;
+    if(bandera){//amex
+    Añobin = parseInt(CC.slice((19) , (24)));
+    Mesbin = parseInt(CC.slice((16) , (19)));
+    if(Añobin > Añoactual || (Añobin == Añoactual && Mesbin > Mesactual)){
+       Verificacionexp = true; 
+       amex(CC,contador);
+    }
+    }else{//general
+    Añobin = parseInt(CC.slice((20),(25)))//inicial,final excluido
+    Mesbin = parseInt(CC.slice((17),(20)))
+    if(Añobin > Añoactual || (Añobin == Añoactual && Mesbin > Mesactual)){
+       Verificacionexp=true;
+        general(CC,contador);
+    }
+    }
+    return Verificacionexp;//primero verificamos fechas que es el proceso mas ligero
 }
-}else{//general
-Añobin=parseInt(CC.slice((20),(25)))//inicial,final excluido
-Mesbin=parseInt(CC.slice((17),(20)))
-if(Añobin>Añoactual ||(Añobin==Añoactual && Mesbin>Mesactual)){
-    Verificacionexp=true;
-}
-}
-}return Verificacionexp 
 
 function amex(CC,contador){ 
-    for(i=(0);i<(15);i++){
-        aux=parseInt(CC[i])
+    for(i = (0) ; i < (15) ; i++){
+        aux = parseInt(CC[i])
         if(i & 1){
-            aux=aux*2
-            if(aux>=10){//verifica caso de 2 digitos
-                aux=(aux-9)
+            aux = aux * 2
+            if(aux >= 10){//verifica caso de 2 digitos
+                aux = (aux - 9)
             }
-            contador=contador+aux;
+            contador = contador + aux;
         }else{
-            contador=contador+aux;
+            contador = contador + aux;
         }
         }//final for
-        if(contador%10!=0){}else{
-            verificarExp()
-        }//si cumple algoritmo luhn se va a verificar fechas 
+        return contador % 10 == 0 ? luhn = true : luhn = false;//luhn retorna si se cumple luhn o no 
 }
 
 function general(CC,contador){
-    for(i=0;i<(16);i++){
-        aux=parseInt(CC[i])
+    for(i = 0 ; i < (16) ; i++){
+        aux = parseInt(CC[i])
             if(i & 0){
-                aux=aux*2
-                if(aux>=10){//verifica caso de 2 digitos
-                    aux=(aux-9)
+                aux = aux*2
+                if(aux >= 10){//verifica caso de 2 digitos
+                    aux = (aux - 9)
                 }
-                contador=contador+aux;
+                contador = contador + aux;
         }else{
-            contador=contador+aux;
+            contador = contador + aux;
         }
     }//final for
-    if(contador%10!=0){}else{
-        verificarExp()
-    } //si cumple luhn se va a verificar fechas
+    return contador % 10 == 0 ? luhn = true : luhn = false;  //luhn retorna si se cumple luhn o no 
 }
