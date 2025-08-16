c04f9a75768a422d680c08237b25ca2ebc6a830f
BoneyC <Boneywzm@gmail.com>
2025-08-16 14:29:54 -0600

    first commit

--- c++/fibonacci.cpp
+++ c++/fibonacci.cpp
@@ -3,7 +3,7 @@
 int main(){
     int Tamanio = 5;
     int Num1 = 1, Num2 = 0,aux;
-    std::string txt="";
+    std::string txt = "";
     for(int i = 0 ; i < Tamanio ; i++){
         txt=txt+std::to_string(Num1)+"\n";
         aux = Num1;
--- c++/luhn.cpp
+++ c++/luhn.cpp
@@ -1,36 +1,42 @@
 #include <iostream>
 #include <string>
 
+int valOrden(int numDigitos);
+int valuhn(bool orden, std::string codigo, int numDigitos);
+
 int main(){
-    bool orden;
     std::string codigo = "4258812475889092";
-    int numDigitos = codigo.length();   
-        if(! (numDigitos & 1)){
-        orden=false;
+    const int numDigitos = codigo.length();   
+    const bool orden = valOrden(numDigitos);// los codigos de length impares usan posiciones impares y los pares posiciones pares 
+    const bool result = valuhn(orden, codigo, numDigitos);// hago que redirija a la funcion 
+    if(result){
+          std::cout<<"el codigo "<<codigo<<" es valido";
     }else{
-        orden = true;
-    } 
+         std::cout<<"el codigo "<<codigo<<" es invalido";
+    }
+}
+
+int valOrden(int numDigitos){
+    return !(numDigitos & 1)? false : true;
+}
+
+int valuhn(bool orden , std::string codigo , int numDigitos){
    int i;
    int auxiliar;
    int suma = 0;
+   bool result;
    std::string digito;
    for( i = 0 ; i < numDigitos ; i++){//desde aca
     digito = codigo[i];
     auxiliar = stoi(digito);
-    if(( i & 1 ) == orden ){/*el "&1" es para saber si es par o impar la posicion y orden para saber*/
-        auxiliar=auxiliar*2;//para seguir la estructura de tre true=true y false false=true 
-        if(auxiliar>9){//"&1" donde estoy y orden en que posicion hacer
-            auxiliar=(auxiliar-9);
+    if(( i & 1 ) == orden){/*el "&1" es para saber si es par o impar la posicion y orden para saber*/
+        auxiliar = auxiliar*2;//para seguir la estructura de tre true=true y false false=true 
+        if(auxiliar > 9){//"&1" donde estoy y orden en que posicion hacer
+            auxiliar = (auxiliar - 9);
         }
     }
-    suma=suma+auxiliar;
+    suma += auxiliar;
    }/* hasta aca estan los calculos que validan luhn lo otro solo es validar dio divisble 
    entre 10 el resto solo es escabilidad a diferentes legth*/
-    //return suma % 10 == 0 ? false : true;
-   if(suma % 10 == 0){
-    return false;
-   }else{
-    return true;
-   }
-   
+   return suma % 10 == 0? result = true : result = false;  
 }
